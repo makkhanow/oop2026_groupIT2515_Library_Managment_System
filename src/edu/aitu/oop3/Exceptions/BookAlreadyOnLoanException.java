91a363a2fcf063401213c8219c83c6f591d10479
elamanorazhan <elamanorazhan0@gmail.com>
2026-01-25 16:59:01 +0500

    assign3

--- src/edu/aitu/oop3/Exceptions/BookAlreadyOnLoanException.java
+++
@@ -1,7 +0,0 @@
-package edu.aitu.oop3.Exceptions;
-
-public class BookAlreadyOnLoanException extends RuntimeException {
-    public BookAlreadyOnLoanException(long bookId) {
-        super("Book is already on loan OR not available: id=" + bookId);
-    }
-}
