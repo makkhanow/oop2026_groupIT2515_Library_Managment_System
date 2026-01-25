f97745ef71fe99914c7c594152983d16b593cb28
elamanorazhan <elamanorazhan0@gmail.com>
2026-01-25 16:59:35 +0500

    assign3

---
+++ src/edu/aitu/oop3/Exceptions/LoanOverdueException.java
@@ -0,0 +1,8 @@
+package edu.aitu.oop3.Exceptions;
+import java.time.LocalDate;
+
+public class  LoanOverdueException extends RuntimeException {
+    public LoanOverdueException(long loanId, LocalDate dueDate) {
+        super("Loan is overdue: loanId=" + loanId + ", dueDate=" + dueDate);
+    }
+}
