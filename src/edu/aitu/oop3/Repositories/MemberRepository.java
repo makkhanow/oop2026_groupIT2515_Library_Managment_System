1fea81b8830ce489de8926d8860850e24c05763e
elamanorazhan <elamanorazhan0@gmail.com>
2026-02-02 00:52:15 +0500

    assign4

--- src/edu/aitu/oop3/Repositories/MemberRepository.java
+++ src/edu/aitu/oop3/Repositories/MemberRepository.java
@@ -6,6 +6,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Optional;
 
-public interface MemberRepository {
+public interface MemberRepository extends Repository<Member, Long> {
     Optional<Member> findById(Connection con, long id) throws SQLException;
 }
